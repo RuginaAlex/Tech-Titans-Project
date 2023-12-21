661a530a3b82ee637c24192db96032e66736ebd3
unknown <alexrugina8@yahoo.com>
2023-12-21 22:03:02 +0200

    SpingBoot implemented

---
+++ src/main/java/com/techtitans/smartbudget/SmartbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.techtitans.smartbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtitans/smartbudget/SmartbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtitans.smartbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
