00594d8a24aeeec07b400982e32f3d72a932306f
Cozma Alexandru <alexcozma1112@gmail.com>
2024-02-12 12:30:44 +0200

    dynamization of data farming

--- src/main/java/com/techtitans/smartbudget/dataManagement/MainDataManagement.java
+++ src/main/java/com/techtitans/smartbudget/dataManagement/MainDataManagement.java
@@ -7,6 +7,7 @@ import com.techtitans.smartbudget.api.model.CompanyData;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -40,7 +41,7 @@ public class MainDataManagement {
                     }
                 });
 
-                Thread.sleep(1000); // Sleep for 1 second
+                Thread.sleep(100); // Sleep for 0.1 seconds
             }
         } catch (Exception e) {
             System.out.println("Background task was interrupted");
@@ -53,26 +54,26 @@ public class MainDataManagement {
     public void addNewDataInDatabase(Companies company) throws IOException {
         String ticker = company.getTicker();
         var date = company.getLast_date_fetched();
-        if(date.getHour() < 13){
+        if (date.getHour() < 13) {
             date = date.withHour(13);
-        }
-        else if(date.getHour() >= 21){
+        } else if (date.getHour() >= 21) {
             date = date.plusDays(1);
             date = date.withHour(13);
-        }
-        else{
+        } else {
             date = date.plusHours(1);
         }
-        if (date.isAfter(LocalDateTime.now())){
+        if (date.isAfter(LocalDateTime.now())) {
             return;
         }
         company.setLast_date_fetched(date);
-        companyController.updateCompany(company.getId_company(),company);
+        companyController.updateCompany(company.getId_company(), company);
 
         OkHttpClient client = new OkHttpClient();
+        var url = getUrl(date, ticker);
 
+        // "+date.getYear()+"-"+date.getMonthValue()+"-"+date.getDayOfMonth()+"T"+date.getHour()+"%3A"+date.getMinute()+"%3A"+date.getSecond()+"&adjustment=raw&feed=iex&currency=EUR&sort=asc"
         Request request = new Request.Builder()
-                .url("https://data.alpaca.markets/v2/stocks/bars?symbols=AAPL&timeframe=1H&start=2021-01-05T00%3A00%3A00Z&end=2021-01-06T00%3A00%3A00Z&adjustment=raw&feed=iex&currency=EUR&sort=asc")
+                .url(url)
                 .get()
                 .addHeader("accept", "application/json")
                 .addHeader("APCA-API-KEY-ID", "PKFCH7AQYMLDBQ455PYQ")
@@ -97,12 +98,50 @@ public class MainDataManagement {
             var time = data.getString("t");
             var timestamp = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME);
 
-            var companyData = new CompanyData(0, company, open, high, low, close,timestamp);
+            var companyData = new CompanyData(0, company, open, high, low, close, timestamp);
 
             companyDataController.createCompanyData(companyData);
 
         }
 
+    }
+
+    @NotNull
+    private static String getUrl(LocalDateTime date, String ticker) {
+        var url = "https://data.alpaca.markets/v2/stocks/bars?symbols=" + ticker + "&timeframe=1H&start=" + date.getYear();
+
+        if (date.getMonthValue() < 10) {
+            url += "-0" + date.getMonthValue();
+        } else {
+            url += "-" + date.getMonthValue();
+        }
+
+        if (date.getDayOfMonth() < 10) {
+            url += "-0" + date.getDayOfMonth();
+        } else {
+            url += "-" + date.getDayOfMonth();
+        }
+
+        if (date.getHour() < 10) {
+            url += "T0" + date.getHour();
+        } else {
+            url += "T" + date.getHour();
+        }
+
+        if (date.getMinute() < 10) {
+            url += "%3A0" + date.getMinute();
+        } else {
+            url += "%3A" + date.getMinute();
+        }
+
+        if (date.getSecond() < 10) {
+            url += "%3A0" + date.getSecond()+"Z";
+        } else {
+            url += "%3A" + date.getSecond()+"Z";
+        }
+
+        url += "&limit=1&adjustment=raw&feed=iex&currency=EUR&sort=asc";
+        return url;
     }
 
 }
